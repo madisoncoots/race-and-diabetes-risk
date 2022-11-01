3e9c53d38363fda30446e385c45f65ffe1bc3855
Madison Elisse Coots <coots.madison@gmail.com>
2022-11-01 06:39:42 -0700

    undid addition of reproductive survey; made cleaning tweaks and updated model formula

--- make_replication_data.R
+++ make_replication_data.R
@@ -13,8 +13,6 @@ raw_body_measurements_11_12 <- read_xpt(paste(path_string, "2011-2012/BMX_G.XPT"
   clean_names()
 raw_glycohemoglobin_11_12 <- read_xpt(paste(path_string, "2011-2012/GHB_G.XPT", sep = "")) %>%
   clean_names()
-raw_reproductive_health_11_12 <- read_xpt(paste(path_string, "2011-2012/RHQ_G.XPT", sep = "")) %>%
-  clean_names()
 
 # 2013-2014
 raw_demographics_13_14 <- read_xpt(paste(path_string, "2013-2014/DEMO_H.XPT", sep = "")) %>%
@@ -24,8 +22,6 @@ raw_survey_responses_13_14 <- read_xpt(paste(path_string, "2013-2014/DIQ_H.XPT",
 raw_body_measurements_13_14 <- read_xpt(paste(path_string, "2013-2014/BMX_H.XPT", sep = "")) %>%
   clean_names()
 raw_glycohemoglobin_13_14 <- read_xpt(paste(path_string, "2013-2014/GHB_H.XPT", sep = "")) %>%
-  clean_names()
-raw_reproductive_health_13_14 <- read_xpt(paste(path_string, "2013-2014/RHQ_H.XPT", sep = "")) %>%
   clean_names()
 
 # 2015-2016
@@ -37,8 +33,6 @@ raw_body_measurements_15_16 <- read_xpt(paste(path_string, "2015-2016/BMX_I.XPT"
   clean_names()
 raw_glycohemoglobin_15_16 <- read_xpt(paste(path_string, "2015-2016/GHB_I.XPT", sep = "")) %>%
   clean_names()
-raw_reproductive_health_15_16 <- read_xpt(paste(path_string, "2015-2016/RHQ_I.XPT", sep = "")) %>%
-  clean_names()
 
 # 2017-2018
 raw_demographics_17_18 <- read_xpt(paste(path_string, "2017-2018/DEMO_J.XPT", sep = "")) %>%
@@ -48,8 +42,6 @@ raw_survey_responses_17_18 <- read_xpt(paste(path_string, "2017-2018/DIQ_J.XPT",
 raw_body_measurements_17_18 <- read_xpt(paste(path_string, "2017-2018/BMX_J.XPT", sep = "")) %>%
   clean_names()
 raw_glycohemoglobin_17_18 <- read_xpt(paste(path_string, "2017-2018/GHB_J.XPT", sep = "")) %>%
-  clean_names()
-raw_reproductive_health_17_18 <- read_xpt(paste(path_string, "2017-2018/RHQ_J.XPT", sep = "")) %>%
   clean_names()
 
 # ------------------- Combine -------------------
@@ -86,18 +78,9 @@ raw_glycohemoglobin_all <- bind_rows(
   raw_glycohemoglobin_17_18
 )
 
-# Reproductive health data
-raw_reproductive_health_all <- bind_rows(
-  raw_reproductive_health_11_12,
-  raw_reproductive_health_13_14,
-  raw_reproductive_health_15_16,
-  raw_reproductive_health_17_18
-)
-
 output_path_string <- "/home/mcoots/harvard/research/race-in-healthcare/data/parsed/"
 
 write_csv(raw_demographics_all, paste(output_path_string, "demographics.csv", sep=""))
 write_csv(raw_survey_responses_all, paste(output_path_string, "survey_responses.csv", sep=""))
 write_csv(raw_body_measurements_all, paste(output_path_string, "body_measurements.csv", sep=""))
 write_csv(raw_glycohemoglobin_all, paste(output_path_string, "glycohemoglobin.csv", sep=""))
-write_csv(raw_reproductive_health_all, paste(output_path_string, "reproductive_health.csv", sep=""))
--- regression_replication.R
+++ regression_replication.R
@@ -10,73 +10,80 @@ raw_body_measurements <- read_csv(paste(data_path, "body_measurements.csv", sep=
 
 raw_glycohemoglobin <- read_csv(paste(data_path, "glycohemoglobin.csv", sep=""))
 
-raw_reproductive_health <- read_csv(paste(data_path, "reproductive_health.csv", sep="")) # Ignore parsing errors -- not w/ col. we want
-
 # Ignoring weights for the first stab at the regression replication
 cleaned_demographics_data <- raw_demographics_data %>%
   select(seqn,
          ridageyr,
-         ridreth3)
-
+         ridreth3,
+         ridexprg) %>%
+  rename(age = ridageyr, 
+         race = ridreth3,
+         pregnant = ridexprg) %>%
+  filter(!is.na(age)) %>% # line 39 of author code
+  filter(age >= 18) %>% # lines 10, 31 of author code
+  filter(age <= 70) %>% # lines 10, 31 of author code
+  mutate(# Making the race variable more readable
+    race = case_when(race == 1 | race == 2 ~ "Hispanic",
+                     race == 3 ~ "White",
+                     race == 4 ~ "Black",
+                     race == 6 ~ "Asian",
+                     race == 7 ~ "Other"), # NOTE TO SELF: MIGHT NEED TO REMOVE THIS CATEG. BEFORE TRAINING!
+    # Converting race to factor
+    race = factor(race),
+    # Re-leveling the race factor, so that White is base level (as in paper)
+    race = relevel(race, ref = "White")) %>%
+  filter(pregnant != 1) %>% # line 32 of author code
+  mutate(age2 = age^2, # line 33 of author code
+         age3 = age^3)
+  
 cleaned_survey_responses_data <- raw_survey_responses %>%
   select(seqn,
-         diq010)
-
+         diq010) %>%
+  filter(!is.na(diq010)) %>% # line 23 of author code
+  mutate(diabetes_diagnosis = (diq010 == 1)) %>%
+  select(-diq010)
+  
 cleaned_body_measurements <- raw_body_measurements %>%
   select(seqn,
-         bmxbmi)
+         bmxbmi) %>%
+  rename(bmi = bmxbmi) %>%
+  filter(!is.na(bmi)) %>% # line 10 of author code
+  filter(bmi >= 18.5) %>% # line 31 of author code (differs from what is said in paper on p. 767)
+  filter(bmi <= 50) %>% # line 31 of author code
+  mutate(bmi2 = bmi^2, # line 34 of author code
+         bmi3 = bmi^3)
 
 cleaned_glycohemoglobin <- raw_glycohemoglobin %>%
-  mutate(high_gh = lbxgh >= 6.5)
-
-cleaned_reproductive_health <- raw_reproductive_health %>%
-  select(seqn,
-         rhd143) # maybe want to do a check at some point to verify response counts posted on NHANES
+  filter(!is.na(lbxgh)) %>% # from paper
+  mutate(high_gh = lbxgh >= 6.5) # this logic taken from paper, not reflected in code
 
 regression_data <- cleaned_demographics_data %>%
   inner_join(cleaned_survey_responses_data, by = c("seqn")) %>%
   inner_join(cleaned_body_measurements, by = c("seqn")) %>%
   inner_join(cleaned_glycohemoglobin, by = c("seqn")) %>%
-  # don't want to drop ppl with NA for high_gh if dr. told them they had diabetes
-  # From the paper: Individuals with missing BMI data (1.2% of our study cohort) 
-  # or hemoglobin A1c values (4.7% of our study cohort) were excluded.
-  # so we might need to undo this depending on what the authors' code looks like 
-  mutate(high_gh_no_NA = if_else(is.na(high_gh) & diq010, 
-                                     TRUE,
-                                 high_gh),
-         has_diabetes = (diq010 == 1) | high_gh,
-         # Making the race variable more readable
-         race = case_when(ridreth3 == 1 | ridreth3 == 2 ~ "Hispanic",
-                          ridreth3 == 3 ~ "White",
-                          ridreth3 == 4 ~ "Black",
-                          ridreth3 == 6 ~ "Asian",
-                          ridreth3 == 7 ~ "Other"),
-         # Converting race to factor
-         race = factor(race),
-         # Re-leveling the race factor, so that White is base level (as in paper)
-         race = relevel(race, ref = "White")) %>%
-  # From the paper: In the model, we excluded adults with extreme BMIs-that is, 
-  # less than 15 kg/m2 or greater than 50 kg/m2 (n = 310) %>%
-  filter(bmxbmi > 15,
-         bmxbmi < 50) %>%
-  select(-lbxgh,
-         -high_gh_no_NA,
-         -high_gh,
-         -ridreth3,
-         -diq010) %>%
-  # Making column names more readable
-  rename(id = seqn,
-         age = ridageyr,
-         bmi = bmxbmi)
+  mutate(has_diabetes = diabetes_diagnosis | high_gh)
+
+# Model formula
+formula <- has_diabetes ~ race + 
+  age + age2 + age3 + 
+  bmi + bmi2 + bmi3 +
+  race:age + race:age2 + race:age3 +
+  race:bmi + race:bmi2 + race:bmi3 +
+  age:bmi
+
 
 # Eventually need to revisit (first, locate) the Supplement so that we can see the *exact* model they ended up using
-model <- glm(has_diabetes ~ age + race + bmi, data = regression_data, family = "binomial")
+model <- glm(formula, data = regression_data, family = "binomial")
 
 summary(model)
 
 test_data <- data.frame(race = c("White", "Asian", "Black", "Hispanic"), 
-                        age = rep(35, 4), 
-                        bmi = rep(25,4))
+                        age = rep(35, 4),
+                        bmi = rep(25,4)) %>%
+  mutate(age2 = age^2,
+         age3 = age^3,
+         bmi2 = bmi^2,
+         bmi3 = bmi^3)
 
 model_pred <- predict(model, newdata = test_data, type = "response")
 
