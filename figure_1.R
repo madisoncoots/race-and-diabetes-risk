1592796afaafc68ff86376410e18309ca41014f6
Madison Elisse Coots <coots.madison@gmail.com>
2022-11-08 18:22:16 -0800

    finished figure 1 (except for some whisker/median issues)

--- figure_1.R
+++ figure_1.R
@@ -16,7 +16,6 @@ figure_data <- raw_demographics_data %>%
   filter(ridageyr >= 35) %>%
   filter(ridageyr <= 70) %>%
   filter(ridexprg != 1 | is.na(ridexprg)) %>%
-  filter(!is.na(diq010)) %>% # line 23 of author code
   rename(age = ridageyr, 
          race = ridreth3) %>%
   mutate(# Making the race variable more readable
@@ -26,85 +25,86 @@ figure_data <- raw_demographics_data %>%
                      race == 4 ~ "Black American",
                      race == 6 ~ "Asian American",
                      race == 7 ~ "Other")) %>%
-  mutate(diabetes_diagnosis = (diq010 == 1)) %>%
-  # filter(!is.na(lbxgh)) %>% # from paper
-  mutate(high_gh = lbxgh >= 6.5) %>% # this logic taken from paper, not reflected in code
-  mutate(has_diabetes = diabetes_diagnosis | high_gh) %>%
-  mutate(wtmec8yr = wtmec2yr/4)
+  mutate(wtmec8yr = wtmec2yr/4) %>%
+  mutate(gender = case_when(riagendr == 1 ~ "Man",
+                            riagendr == 2 ~ "Woman",
+                            TRUE ~ "Missing")) %>%
+  mutate(lbxgh = as.numeric(as.character((lbxgh))),
+         diq010 = as.numeric(as.character((diq010))),
+         a1c = cut(lbxgh,breaks=c(0,5.7,6.5,1000),right=FALSE),
+         diabetes_diagnosis = case_when(diq010 %in% 1 ~ 1, 
+                                        diq010 %in% c(2,3,9) ~ 0,
+                                        diq010 %in% 7 ~ as.numeric(NA)),
+         diabetes = diabetes_diagnosis,
+         diabetes = if_else(a1c=="[6.5,1e+03)" &!is.na(a1c), 1, diabetes)
+  )
 
-NHANES_design <- svydesign(
-  data = figure_data, 
-  strata = ~sdmvstra, 
-  ids = ~sdmvpsu, 
-  nest = TRUE, 
-  weights = ~wtmec8yr
-)
+# NHANES_design <- svydesign(
+#   data = figure_data, 
+#   strata = ~sdmvstra, 
+#   ids = ~sdmvpsu, 
+#   nest = TRUE, 
+#   weights = ~wtmec8yr
+# )
 
-svyquantile(x = ~race + bmxbmi, 
-            design = NHANES_design, 
-            na.rm = TRUE, 
-            quantiles = c(.50))
-svyby(formula = ~bmxbmi, by = ~race, FUN = svymean, 
-      design = NHANES_design, na.rm = TRUE, keep.names = FALSE) %>%
-  ggplot(aes(x = race, y = bmxbmi)) + geom_boxplot()
+# svyquantile(x = ~race + bmxbmi, 
+#             design = NHANES_design, 
+#             na.rm = TRUE, 
+#             quantiles = c(.50))
+# svyby(formula = ~bmxbmi, by = ~race, FUN = svymean, 
+#       design = NHANES_design, na.rm = TRUE, keep.names = FALSE) %>%
+#   ggplot(aes(x = race, y = bmxbmi)) + geom_boxplot()
 
 
 tukey_thresholds <- tukey_outlier(figure_data$bmxbmi)
 
-figure_data %>%
-  filter(race != "Other") %>%
-  filter(!is.na(bmxbmi)) %>%
-  filter(has_diabetes) %>%
-  # filter(bmxbmi >= tukey_thresholds['bottom_threshold'],
-  #        bmxbmi <= tukey_thresholds['top_threshold']) %>%
-  group_by(race) %>%
-  dplyr::summarize(median_bmi = median(bmxbmi))
+# For later sanity checks....
+# figure_data %>%
+#   filter(race != "Other") %>%
+#   filter(!is.na(bmxbmi)) %>%
+#   filter(diabetes == 1) %>%
+#   filter(bmxbmi >= tukey_thresholds['bottom_threshold'],
+#          bmxbmi <= tukey_thresholds['top_threshold']) %>%
+#   group_by(race) %>%
+#   dplyr::summarize(median_bmi = median(bmxbmi))
 
 figure_data %>%
   filter(race != "Other") %>%
   filter(!is.na(bmxbmi)) %>%
-  filter(diabetes_diagnosis) %>%
-  # filter(bmxbmi > tukey_thresholds['bottom_threshold'],
-  #        bmxbmi < tukey_thresholds['top_threshold']) %>%
-  group_by(race) %>%
-  ggplot(aes(x=race, y=bmxbmi, weight = wtmec8yr)) + 
-  geom_boxplot()
-
-
-
-
-svyboxplot(~bmxbmi~ridreth3,
-           subset(df_survey_final,ridageyr>=35 & ridageyr<=70  & diabetes==1)
-           ,ylim=c(0,60),xlab="Race/Ethnicity",ylab="Body Mass Index",
-           col=c("slateblue"),
-           xaxt="n",xlim=c(0.5,5.3),range=0,yaxt="n",cex.lab=1.5,cex.main=2,
-           main="Body Mass Index of Individuals with Diabetes"
-)
-
-axis(1,at=1:6,font=2,
-     labels=c(" White \n American","Mexican \n American","Hispanic \n American",
-              "Black \n American","Asian \n American","Other"),cex.axis=1.1
-     ,tick=FALSE)
-axis(2,at=seq(0,60,10),font=2,
-     las=2,labels=c("0","10","20","30","40","50","60"),cex.axis=1.2)
-
-# These match
-df %>% count(ridreth3)
-figure_data %>% count(ridreth3)
-
-df %>% group_by(ridreth3) %>%
-  filter(!ridexprg==1 |is.na(ridexprg)) %>%
-  filter(!is.na(bmxbmi)) %>%
-  filter(ridageyr>=35) %>%
-  filter(ridageyr<= 70) %>%
   filter(diabetes == 1) %>%
-  dplyr::summarize(med_bmi = median(bmxbmi))
-
-df_survey$variables$bmxbmi
-df_survey$variables$ridreth3
-data.frame(race = df_survey$variables$ridreth3, 
-           bmi = df_survey$variables$bmxbmi,
-           age = ) %>%
+  filter(bmxbmi > tukey_thresholds['bottom_threshold'],
+         bmxbmi < tukey_thresholds['top_threshold']) %>%
   group_by(race) %>%
-  filter(!is.na(bmi)) %>%
-  dplyr::summarise(med_bmi = median(bmi))
+  mutate(race = factor(race, c("White American", 
+                               "Asian American",
+                               "Black American",
+                               "Mexican American",
+                               "Other Hispanic American"))) %>%
+  ggplot(aes(x=race, y=bmxbmi, weight = wtmec8yr)) + 
+  geom_boxplot(fill="darkgray", outlier.shape = NA) +
+  theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ylim(c(0,60)) +
+  labs(x = "Race/Ethnicity",
+       y = "BMI, kg/m^2",
+       title = "BMI among U.S. adults with diabetes,\nstratified by race/ethnicity.") +
+  scale_x_discrete(labels=c("White American" = "White\nAmerican",
+                            "Asian American" = "Asian\nAmerican",
+                            "Black American" = "Black\nAmerican",
+                            "Mexican American" = "Mexican\nAmerican",
+                            "Other Hispanic American" = "Other\nHispanic\nAmerican")) +
+  theme(axis.text.x = element_text(face="bold", size=12),
+        axis.text.y = element_text(face="bold", size=12))
+
+save_path <- "/home/mcoots/harvard/research/race-in-healthcare/replication_figs/"
+ggsave(paste(save_path, "bmi_boxplot.png"), height = 8, width = 6)
+
+# For later sanity checks...
+# df %>% filter(!is.na(bmxbmi)) %>%
+#   filter(diabetes == 1) %>%
+#   filter(bmxbmi > tukey_thresholds['bottom_threshold'],
+#          bmxbmi < tukey_thresholds['top_threshold']) %>%
+#   group_by(ridreth3) %>%
+#   ggplot(aes(x=ridreth3, y=bmxbmi, weight = wtmec8yr)) + 
+#   geom_boxplot(outlier.shape = NA) +
+#   labs(x = "Race")
